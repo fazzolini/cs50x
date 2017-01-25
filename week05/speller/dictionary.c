01d041b7e0eaeab372eb47f3ff63962030eab30e
Maxim Pechyonkin <fazzolini@gmail.com>
2017-01-25 17:06:16 +0000

    imports, bool, trie node and 2 prototypes

--- week05/speller/dictionary.c
+++ week05/speller/dictionary.c
@@ -1,17 +1,50 @@
-/**
- * Implements a dictionary's functionality.
+/**----------------------------------------------------------------------------
+ * Do all imports
  */
-
 #include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #include "dictionary.h"
 
+/**----------------------------------------------------------------------------
+ * Define structs
+ */
+// boolean type
+typedef enum { false, true } bool;
+
+// define a node for trie
+typedef struct _node
+{
+    bool is_word;
+    struct _node *children[27];
+}
+node;
+
+/**----------------------------------------------------------------------------
+ * Global vars to be accessed by all methods
+ */
+node *root;
+
+/** ---------------------------------------------------------------------------
+ * prototypes
+ */
+int get_trie_index(int c); // return index in trie based on character
+void add_word(node *trie_root, char *word); // add word to trie
+
+/**----------------------------------------------------------------------------
+ * ----------------------------------------------------------------------------
+ * Implement functionality
+ */
+ 
 /**
  * Returns true if word is in dictionary else false.
  */
 bool check(const char *word)
 {
     // TODO
+    printf("root address is: %p\n", root);
+    printf("root value is %d\n", *root);
     return false;
 }
 
@@ -21,6 +54,10 @@ bool check(const char *word)
 bool load(const char *dictionary)
 {
     // TODO
+    
+    root = malloc(sizeof(int));
+    *root = 42;
+    
     return false;
 }
 
