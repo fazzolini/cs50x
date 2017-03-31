a7e97b1524d6d0feae72771004ca6c87c51034d8
Maxim Pechyonkin <fazzolini@gmail.com>
2017-03-31 11:24:11 +0000

    pset6: implemented analyzer.py; smile works

--- week08/pset6/sentiments/analyzer.py
+++ week08/pset6/sentiments/analyzer.py
@@ -5,11 +5,42 @@ class Analyzer():
 
     def __init__(self, positives, negatives):
         """Initialize Analyzer."""
+        # initialize tokenizer
+        self.tokenizer = nltk.tokenize.TweetTokenizer()
+        
+        # create the set of positive words
+        self.positives = set()
+        with open(positives) as lines:
+            for line in lines:
+                if (not line.startswith(";")) and (not line.startswith("\n")):
+                    line = line.strip("\n ")
+                    self.positives.add(line.lower())
+                    
+        # create the set of negative words
+        self.negatives = set()
+        with open(negatives) as lines:
+            for line in lines:
+                if (not line.startswith(";")) and (not line.startswith("\n")):
+                    line = line.strip("\n ")
+                    self.negatives.add(line.lower())
+        
+        # check number of words in each set of words       
+        # print("positives: {}".format(len(self.positives)))
+        # print("negatives: {}".format(len(self.negatives)))
 
-        # TODO
 
     def analyze(self, text):
         """Analyze text for sentiment, returning its score."""
-
-        # TODO
-        return 0
+        # initialize
+        tokens = self.tokenizer.tokenize(text)
+        score = 0
+        
+        # assess each word in text
+        for t in tokens:
+            if t.lower() in self.positives:
+                score += 1
+            if t.lower() in self.negatives:
+                score -= 1
+        
+        # return total score for text
+        return score
